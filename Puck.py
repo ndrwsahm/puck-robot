d46a27ea0b0e24297398cb5c01c4013f9aeee64d
Andrew <andrew_sahm@yahoo.com>
2024-11-19 11:57:26 -0500

    Added throttle back

--- Puck.py
+++ Puck.py
@@ -17,14 +17,14 @@ if __name__ == "__main__":
 
     while True:
        # pca.servo[P.LEFT_MOTOR_PIN].angle = 180
-        pca.servo[1].angle = 180
-        #pca.continuous_servo[P.LEFT_MOTOR_PIN].throttle = 1
-        #pca.continuous_servo[P.RIGHT_MOTOR_PIN].throttle = 1
+        #pca.servo[1].angle = 180
+        pca.continuous_servo[P.LEFT_MOTOR_PIN].throttle = 1
+        pca.continuous_servo[P.RIGHT_MOTOR_PIN].throttle = 1
         time.sleep(1)
-        #pca.continuous_servo[P.LEFT_MOTOR_PIN].throttle = -1
-        #pca.continuous_servo[P.RIGHT_MOTOR_PIN].throttle = -1
+        pca.continuous_servo[P.LEFT_MOTOR_PIN].throttle = -1
+        pca.continuous_servo[P.RIGHT_MOTOR_PIN].throttle = -1
         time.sleep(1)
         #pca.servo[P.LEFT_MOTOR_PIN].angle = 0
-        pca.servo[1].angle = 0
-        #pca.continuous_servo[P.LEFT_MOTOR_PIN].throttle = 0
-        #pca.continuous_servo[P.RIGHT_MOTOR_PIN].throttle = 0
+        #pca.servo[1].angle = 0
+        pca.continuous_servo[P.LEFT_MOTOR_PIN].throttle = 0
+        pca.continuous_servo[P.RIGHT_MOTOR_PIN].throttle = 0
