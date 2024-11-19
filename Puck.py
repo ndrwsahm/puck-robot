48edcc696c6289a990e67134980fea783737817f
Andrew <andrew_sahm@yahoo.com>
2024-11-19 11:47:30 -0500

    Added servo angle

--- Puck.py
+++ Puck.py
@@ -13,11 +13,15 @@ if __name__ == "__main__":
     setup()
 
     while True:
+        pca.servo[0].angle = 180
+        pca.servo[1].angle = 180
         pca.continuous_servo[P.LEFT_MOTOR_PIN].throttle = 1
         pca.continuous_servo[P.RIGHT_MOTOR_PIN].throttle = 1
         time.sleep(1)
         pca.continuous_servo[P.LEFT_MOTOR_PIN].throttle = -1
         pca.continuous_servo[P.RIGHT_MOTOR_PIN].throttle = -1
         time.sleep(1)
+        pca.servo[0].angle = 0
+        pca.servo[1].angle = 0
         pca.continuous_servo[P.LEFT_MOTOR_PIN].throttle = 0
         pca.continuous_servo[P.RIGHT_MOTOR_PIN].throttle = 0
