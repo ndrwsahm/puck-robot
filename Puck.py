c5b49c16659ecdf64c1950d0b9d273ccfd7d3d9b
Andrew <andrew_sahm@yahoo.com>
2024-11-19 11:58:21 -0500

    Added longer delay

--- Puck.py
+++ Puck.py
@@ -20,11 +20,12 @@ if __name__ == "__main__":
         #pca.servo[1].angle = 180
         pca.continuous_servo[P.LEFT_MOTOR_PIN].throttle = 1
         pca.continuous_servo[P.RIGHT_MOTOR_PIN].throttle = 1
-        time.sleep(1)
+        time.sleep(3)
         pca.continuous_servo[P.LEFT_MOTOR_PIN].throttle = -1
         pca.continuous_servo[P.RIGHT_MOTOR_PIN].throttle = -1
-        time.sleep(1)
+        time.sleep(3)
         #pca.servo[P.LEFT_MOTOR_PIN].angle = 0
         #pca.servo[1].angle = 0
         pca.continuous_servo[P.LEFT_MOTOR_PIN].throttle = 0
         pca.continuous_servo[P.RIGHT_MOTOR_PIN].throttle = 0
+        time.sleep(3)
