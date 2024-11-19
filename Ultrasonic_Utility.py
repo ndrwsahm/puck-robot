16a9c6ba40fbb0e201a41828c5c207b4a30fb86e
Andrew <andrew_sahm@yahoo.com>
2024-11-19 14:24:41 -0500

    Added ultrasonic sensor

---
+++ Ultrasonic_Utility.py
@@ -0,0 +1,70 @@
+import RPi.GPIO as GPIO
+import Parameters as P
+import time
+
+"""
+==========================================================================
+function : ultrasonic_init
+
+description: Sets the initial settings for the ultrasonic sensors
+
+Inputs
+----------
+
+Outputs
+---------
+pca  :  object  :  object for the pca motor controller
+==========================================================================
+"""
+def ultrasonic_init():
+
+    GPIO.setmode(GPIO.BCM)
+
+    # Set triggers as outputs
+    GPIO.setup(P.FRONT_LEFT_TRIGGER_PIN, GPIO.OUT)
+    GPIO.setup(P.FRONT_RIGHT_TRIGGER_PIN, GPIO.OUT)
+    GPIO.setup(P.BACK_LEFT_TRIGGER_PIN, GPIO.OUT)
+    GPIO.setup(P.BACK_RIGHT_TRIGGER_PIN, GPIO.OUT)
+
+    # Set echos as inputs
+    GPIO.setup(P.FRONT_LEFT_ECHO_PIN, GPIO.IN)
+    GPIO.setup(P.FRONT_RIGHT_ECHO_PIN, GPIO.IN)
+    GPIO.setup(P.BACK_LEFT_ECHO_PIN, GPIO.IN)
+    GPIO.setup(P.BACK_RIGHT_ECHO_PIN, GPIO.IN)
+
+"""
+==========================================================================
+function : ultrasonic_distance
+
+description: Sets the initial settings for the ultrasonic sensors
+
+Inputs
+----------
+
+Outputs
+---------
+pca  :  object  :  object for the pca motor controller
+==========================================================================
+"""
+def ultrasonic_distance(ultrasonic_sensor):
+    GPIO.output(ultrasonic_sensor[P.TRIGGER], True)
+
+    time.sleep(0.00001)
+
+    GPIO.output(ultrasonic_sensor[P.TRIGGER], False)
+
+    start_time = time.time()
+    stop_time = time.time()
+
+    # save start time
+    while GPIO.input(ultrasonic_sensor[P.ECHO]) == 0:
+        start_time = time.time()
+
+    # save time of arrival
+    while GPIO.input(ultrasonic_sensor[P.ECHO]) == 1:
+        stop_time = time.time()
+
+    elapased_time = stop_time - start_time
+    distance = (elapased_time * 34300) / 2
+
+    return distance
